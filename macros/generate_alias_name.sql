212759445c859bbbfb6ffa8afc8429b7279a26f1
AbhiRaj40 <unraja40@gmail.com>
2023-01-28 16:57:25 +0530

    new

--- macros/generate_alias_name.sql
+++ macros/generate_alias_name.sql
@@ -1,8 +1,8 @@
-{% macro generate_alias_name(custom_alias_name=none, node=nore) %}
+{% macro generate_alias_name(custom_alias_name=none, node=none) %}
     {% set unit_test_folder = 'zz_unit_test' %}
 
     {%- if custom_alias_name is none and node.name is none -%}
-        {{ exceptions.raise_compiler_error("!!! Both custom alias name and node name can not be empty") }}
+        {{ exceptions.raise_compiler_error("!!!Error!!! Both custom alias name and node name can not be empty") }}
     {%- endif -%}
 
     {%- if custom_alias_name is none -%}
@@ -14,7 +14,7 @@
             {% if unit_test_folder in node.fqn %}
                 {{ node.name | trim }}
             {% else %}
-                {{ exceptions.raise_compiler_error("Invalid model name, should have pattern like this <m or v or i>_<folder>__<entity>.sql. Issue with model - " + '/'.join(node.fqn)) }}
+                {{ exceptions.raise_compiler_error("Invalid model name, should have pattern like this <m or v or i>_<folder>__<entity>.sql. Issue with model -  " + '/'.join(node.fqn)) }}
             {% endif %}
         {% endif %}
 
--- macros/generate_delete_query.sql
+++ macros/generate_delete_query.sql
@@ -6,7 +6,7 @@
     {%- set ns = namespace(query = 'false', source_tables_region = [], source_tables_non_region = []) -%}
     {%- set ecc_regions = ['SAP_ECC_MANA','SAP_ECC_MXOT'] -%}
 
-    {# separate sources into two categories - Tables that are available in 7 regions and tables available in single regions #}
+    {# segregate sources in to two categories - Tables that are available in 7 regions and tables available in single regions #}
 
     {%- for i in sources -%}
         {%- if 'region' in i -%}
@@ -26,11 +26,11 @@
 
     {%- for i in ns.source_table_non_region -%}
         {%- set i = i.replace(' ','') -%}
-        {%- set db_table = i.replace("'","").split('(')[1].split(')')[0].split(',') -%}
+        {%- set db_table = i.replace("'", "").split('(')[1].split(')')[0].split(',') -%}
         {%- set db_name = db_table[0] -%}
         {%- set table_name = db_table[1] -%}
         {%- set var_val = source(db_name, table_name) | string -%}
-        {%- set ns.query = ns.query + db_name + "_" + table_name + " as (select * from " + var_val + " where RECORDSTATUS != 'D' ), \n" -%}
+        {%- set ns.query = ns.query + db_name + "_" + table_name + " as(select * from " + var_val + " where RECORDSTATUS != 'D' ),\n" -%}
     {%- endfor -%}
 
     {# use loop to generate query for all seven regions #}
@@ -42,8 +42,8 @@
         {%- set db_name = db_table[0] -%}
         {%- set table_name = db_table[1] -%}
         {%- set var_val = source(region, table_name) | string -%}
-        {%- set ns.query = ns.query + region + "_" + table_name + " as (select * from " + var_val + " where RECORDSTATUS != 'D' ), \n" -%}
+        {%- set ns.query = ns.query + region + "_" + table_name + " as(select * from " + var_val + " where RECORDSTATUS != 'D' ),\n" -%}
     {%- endfor -%}
     {%- endfor -%}
     {{- ns.query -}}
-{%- endmacro -%
+{%- endmacro -%}

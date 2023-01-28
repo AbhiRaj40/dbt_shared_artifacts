aa9f23df95673fef5e4fdb11cb3a9f852f5e3285
AbhiRaj40 <unraja40@gmail.com>
2023-01-28 17:22:13 +0530

    new

--- macros/source.sql
+++ macros/source.sql
@@ -1,9 +1,11 @@
 {%- macro source(source_name = none, table_name = none) -%}
     {% set test_profile = 'unit_test' %}
     {%- set table_dup = ['EKKO','EKPO','MARA','MARC','LFA1','LFB1','BKPF','BSEG'] -%}
+
     {%- if source_name is none -%}
         {{ exceptions.raise_compiler_error("!!!Error!!! Source name can not be empty") }}
     {%- endif -%}
+
     {%- if table_name is none -%}
         {{ exceptions.raise_compiler_error("!!!Error!!! Table name can not be empty") }}
     {%- endif -%}
@@ -11,7 +13,7 @@
     {%- if target.name == test_profile -%}
         {%- if table_name in table_dup and source_name == 'SAP_MDG' -%}
             {{- ref(table_name + '_mdg_input') -}}
-        {%- if table_name in table_dup and source_name == 'SAP_MDG_1' -%}
+        {%- elif table_name in table_dup and source_name == 'SAP_MDG_1' -%}
             {{- ref(table_name + '_mdg_input') -}}
         {%- else -%}
             {{- ref(table_name + '_input') -}}
